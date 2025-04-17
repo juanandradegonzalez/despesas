d983a753a2b0947c25eab7ecc821a151df16c40a
Juan Gonzalez <andrade.gonzalez.sfc@gmail.com>
2025-04-17 14:45:14 -0300

    Descrição do seu commit

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,15 +1,14 @@
+
 // db.js
-const mysql = require('mysql2/promise');
+const { Pool } = require('pg');  // Importando a biblioteca para PostgreSQL
 require('dotenv').config();
 
-const pool = mysql.createPool({
+const pool = new Pool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
+  port: 5432,  // Porta padrão do PostgreSQL
 });
 
 module.exports = pool;
